0a442242483668c260631bc2eaba6e40d20d654d
Yll <yll.kelani@hotmail.co.uk>
2016-11-04 12:25:06 +0000

    testing

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 var sessionUsername;
 
 function httpGetAsync(url, type, message, callback) {
+    console.log("ok");
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState === 4) {
