54425e123aa2d7010c10525d393259c67bbf6c89
Aksel CAKMAK <akselcakmak@gmail.com>
2016-11-04 05:16:04 +0000

    updated some comments

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,9 @@ function tableCallback(data) {
     console.log(Object.keys(data).length);
     var out = "<table>";
     out += " <tr> <th>id</th> <th>title</th> <th>body</th> </tr>"
+    // For each objects in the JSON response, create a table entry with some properties (eg: id, title, body ect..)
     for (var i = 0; i < Object.keys(data).length; i++) {
+        // TODO: When we'll use the actual API of our server, change these properties to the actual properties of the DB.
         out += "<tr><td>" +
             data[i].id +
             "</td><td>" +
@@ -66,43 +68,34 @@ function getRefDetails() {
 
 function submitRef() {
     var reference = getRefDetails();
-    // todo: PUT a new reference via Jay's RESTful API
     httpGetAsync("https://jsonplaceholder.typicode.com/posts", "PUT", reference);
 }
 
 function removeRef() {
     var reference = getRefDetails();
-    // todo: DELETE a reference (using its unique id) via the RESTful API
     httpGetAsync("https://jsonplaceholder.typicode.com/posts", "DELETE", reference);
 
 }
 
 function submitNewUser() {
     var username = document.getElementById('username').value;
-    // var lastName = document.getElementById('last_name').value;
-    // var emailAddress = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-
 
     var newDetails = {
         "username": username,
         "password": password
     };
 
-    // todo: PUT a new user via the API
     httpGetAsync("https://jsonplaceholder.typicode.com/posts", "PUT", newDetails);
 
 }
 
 
 function showAllRef() {
-    // GET all the references via the API
     var references = httpGetAsync("https://jsonplaceholder.typicode.com/posts", "GET", null, tableCallback);
-
-    
-    // todo: for each reference in references -> <li> the reference </li> or smthing
+    // The <table> listing is done in the tableCallback function
 }
-window.onload = showAllRef; // This ensures that showAllRef() shows a table as soon as the main page loads
+window.onload = showAllRef; // This ensures that showAllRef() shows the table info as soon as the main page loads
 
 
 $(function() {
